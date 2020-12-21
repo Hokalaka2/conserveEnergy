78adcf482dbfc1c23648e7ccd3875daf94b334d0
Otis Milliken <otis.true@gmail.com>
2020-12-21 14:15:37 -0700

    Setup mvc framework for project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var port = process.env.port || 3000;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
